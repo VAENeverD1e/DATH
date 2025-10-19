25b8cec3963233eb8a1fbb69e18832b4029af396
KoangWy <bohecsoc@gmail.com>
2025-10-19 14:06:14 +0700

    add doctor dashboard

--- src/App.js
+++ src/App.js
@@ -5,43 +5,54 @@ import Register from "./pages/Register";
 import HomePage from "./pages/HomePage";
 import FloatingMenu from "./components/FloatingMenu";
 import AdminDashboard from "./pages/AdminDashboard";
-import { Home } from "lucide-react";
+import DoctorDashboard from "./pages/DoctorDashboard";
+import ServicePage from "./Service";
+// import { Home } from "lucide-react"; // bỏ vì không dùng
 
 export default function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/AdminDashboard" element={<AdminDashboard />} />
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <HomePage />
-              </Layout>
-            }
-            />
-          <Route 
-            path="/register" 
-            element={
-              <Layout>
-                <Register />
-              </Layout>
-            }
-              />
-            <Route
-              path="/services"
-              element={
-                <Layout>
-                  <ServicePage />
-                </Layout>
-            }
-            />
+      <Routes>
+        {/* routes không bọc layout */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* routes bọc layout */}
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <HomePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <Layout>
+              <AdminDashboard />
+            </Layout>
+          }
+        />
+        <Route
+          path="/services"
+          element={
+            <Layout>
+              <ServicePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/doctor"
+          element={
+            <Layout>
+              <DoctorDashboard />
+            </Layout>
+          }
+        />
       </Routes>
 
       <FloatingMenu />
-
     </Router>
   );
 }
