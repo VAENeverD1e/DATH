4dffd703f0652a3d5e8e405143d132b22451653f
codemiy282 <hungle28225@gmail.com>
2025-10-20 13:01:16 +0700

    Delete Patient_Doctor_Dashboard directory

--- Patient_Doctor_Dashboard/src/routes/DoctorDashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { DoctorDashboard } from "./screens/DoctorDashboard";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <DoctorDashboard />
-  </StrictMode>,
-);
