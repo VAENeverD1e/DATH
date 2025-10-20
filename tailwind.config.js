877fa8190640d14857a084e981842b1bd90c34cb
codemiy282 <hungle28225@gmail.com>
2025-10-20 13:37:25 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui'],
+        display: ['Playfair Display', 'serif'],
+      },
+      colors: {
+        'brand-deep': '#0f2340',
+        'brand-accent': '#b9374a'
+      }
+    },
   },
   plugins: [],
 };
